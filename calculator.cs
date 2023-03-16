e71b9816673a67ca4032c2297f7560b16dffca42
wisnalaa <wisnalaa@students.amikom.ac.id>
2023-03-16 15:02:12 +0700

    melengkapi fungsi main lagi

--- calculator.cs
+++ calculator.cs
@@ -7,6 +7,8 @@ class Calculator
 		
 		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
 		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		Console.WriteLine("Hasil Perkalian: {0} * {1} = {2}", a, b, Perkalian(a, b));
+		Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}", a, b, Pembagian(a, b));
 		
 		Console.WriteLine("\nTekan sembarang key untuk keluar");
 		Console.ReadKeyey();
@@ -16,8 +18,19 @@ class Calculator
 	{
 		return a + b;
 	}
+	
 	static int Pengurangan(int a, int b)
 	{
 		return a - b;
+	}
+	
+		static int Perkalian(int a, int b)
+	{
+		return a * b;
+	}
+	
+		static int Pembagian(int a, int b)
+	{
+		return a / b;
 	}
 }
